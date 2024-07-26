762d73cf973d3f560254cf43c66de938d16b5b4e
shivkumar-github <shivkumarrajmane164@gmail.com>
2024-07-27 02:38:00 +0530

    commit3

--- code with harry ultimate pythone one shot cours/Mega Project 1 Jarvis/client.py
+++ code with harry ultimate pythone one shot cours/Mega Project 1 Jarvis/client.py
@@ -1,11 +1,5 @@
-from openai import OpenAI
-
-client = OpenAI()
-# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
 # if you saved the key under a different environment variable name, you can do something like:
-client = OpenAI(
-  api_key= "sk-proj-xaCMxsuyafaf4hdD5N0bT3BlbkFJqZpOmplANde899QA3Dg7"
-)
+
 
 completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
