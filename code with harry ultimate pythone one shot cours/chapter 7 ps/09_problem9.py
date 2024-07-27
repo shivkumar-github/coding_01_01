b2a627276dc82800e222ea3238b1d3a3519872d0
shivkumar-github <shivkumarrajmane164@gmail.com>
2024-07-27 15:03:05 +0530

    add another approach for looping question

--- code with harry ultimate pythone one shot cours/chapter 7 ps/09_problem9.py
+++ code with harry ultimate pythone one shot cours/chapter 7 ps/09_problem9.py
@@ -1,30 +1,37 @@
-# ***
-# * *
-# ***
-# for n = 3
-spaceno = 0
-n = int(input("Enter the number : "))
+# # ***
+# # * *
+# # ***
+# # for n = 3
+# spaceno = 0
+# n = int(input("Enter the number : "))
 
-# dividing in 3 parts 
-print("*" * n)
+# # dividing in 3 parts 
+# print("*" * n)
 
-spaceno = 1
-for i in range(1, int(n/2) ):
-        starno = int((n - spaceno)/2)
-        print("*" * starno, end="")
-        print(" " * spaceno, end="")
-        print("*" * starno)
-        spaceno+=2 
+# spaceno = 1
+# for i in range(1, int(n/2) ):
+#         starno = int((n - spaceno)/2)
+#         print("*" * starno, end="")
+#         print(" " * spaceno, end="")
+#         print("*" * starno)
+#         spaceno+=2 
         
-print("*" + " " * (n-2) + "*")
+# print("*" + " " * (n-2) + "*")
 
-starno = 2
-for i in range(1, int(n/2) ):
-        spaceno = n - starno*2
-        # print(starno, spaceno)
-        print("*" * starno, end="")
-        print(" " * spaceno, end="")
-        print("*" * starno)
-        starno+=1
+# starno = 2
+# for i in range(1, int(n/2) ):
+#         spaceno = n - starno*2
+#         # print(starno, spaceno)
+#         print("*" * starno, end="")
+#         print(" " * spaceno, end="")
+#         print("*" * starno)
+#         starno+=1
                 
-print("*" * n)
+# print("*" * n)
+
+
+n = 5
+print("*"*n)
+for i in range (n-2) : 
+        print("*" + " "*(n-2) + "*")
+print("*"*n)
